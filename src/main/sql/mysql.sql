22c7cd810873e2fd0adc6bfae753306da921b4bd
wqsccdhy-wq <32351704+wqsccdhy@users.noreply.github.com>
2019-10-24 21:56:48 +0800

    sql语句提交

--- src/main/sql/mysql.sql
+++ src/main/sql/mysql.sql
@@ -20,3 +20,22 @@ CREATE TABLE behavior_statistics_unit_govdoc (
   KEY BEHAVIOR_STAT_INDEX (YEAR,MONTH,CON_VALUE),
   KEY BEHAVIOR_STAT_UNIT_INDEX (UNIT_ID)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE client_info (
+  id bigint(20) NOT NULL AUTO_INCREMENT,
+  client_id varchar(255) DEFAULT NULL,
+  client_secret varchar(255) DEFAULT NULL,
+  client_name varchar(255) DEFAULT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE user_info (
+  user_id int(11) NOT NULL,
+  user_name varchar(255) NOT NULL,
+  user_password varchar(255) NOT NULL,
+  user_describe varchar(255) DEFAULT NULL COMMENT '描述',
+  create_time datetime DEFAULT NULL,
+  update_time datetime DEFAULT NULL,
+  PRIMARY KEY (user_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
